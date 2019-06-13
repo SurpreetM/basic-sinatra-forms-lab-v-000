5ea093df664f3de44b4b1eec2072c2259cfd2f82
surpreet.mundra <surpreet.mundra@gmail.com>
2019-06-13 17:32:01 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -7,7 +7,6 @@ class App < Sinatra::Base
   end
 
   post '/newteam' do
-
     @name = params[:name]
     @coach = params[:coach]
     @pg = params[:pg]
@@ -15,7 +14,6 @@ class App < Sinatra::Base
     @sf = params[:sf]
     @pf = params[:pf]
     @c = params[:c]
-
     erb :team
   end
 
